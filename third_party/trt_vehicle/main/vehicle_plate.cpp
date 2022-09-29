1a2fd96d73e5c239eac9d55a633db10862602190
sherlockchou86 <zhouzhi9@foxmail.com>
2022-09-29 17:16:04 +0800

    commit-6 9/29

--- third_party/trt_vehicle/main/vehicle_plate.cpp
+++ third_party/trt_vehicle/main/vehicle_plate.cpp
@@ -1,4 +1,5 @@
 #include <opencv2/core/core.hpp>
+#include <opencv2/freetype.hpp>
 #include <string>
 #include <vector>
 
@@ -14,20 +15,24 @@ using namespace std;
 #if VEHICLE_PLATE
 
 std::map<std::string, cv::Scalar> colors {{"blue", cv::Scalar(255, 0, 0)},
-                                        {"yellow", cv::Scalar(0, 255, 255)},
                                         {"green", cv::Scalar(0, 255, 0)},
+                                        {"yellow", cv::Scalar(0, 255, 255)},
                                         {"white", cv::Scalar(255, 255, 255)}};
 
 int main(){
-    std::string plateModelPath = "../data/model/plate/det.trt";
+    std::string plateModelPath = "../data/model/plate/det_2.trt";
     std::string charModelPath = "../data/model/plate/rec.trt";
-    auto image = cv::imread("../data/test/plate/truck2.png");
+    auto image = cv::imread("../data/test/plate/truck3.png");
     auto image2 = cv::imread("../data/test/plate/3in1.png");
+    auto image3 = cv::imread("../data/test/plate/truck2.png");
     auto plateDetector = new trt_vehicle::VehiclePlateDetector(plateModelPath, charModelPath);
 
+    auto ft2 = cv::freetype::createFreeType2();
+    ft2->loadFontData("../data/font/NotoSansCJKsc-Medium.otf", 0);
+
     std::vector<std::vector<trt_vehicle::Plate>> results;
-    std::vector<cv::Mat> img_datas = {image, image2};
-    plateDetector->detect(img_datas, results, true);
+    std::vector<cv::Mat> img_datas = {image, image2, image3};
+    plateDetector->detect(img_datas, results);
 
     for (int i = 0; i < results.size(); i++) {
         /* code */
@@ -35,13 +40,15 @@ int main(){
         {
             /* code */
             auto& plate = results[i][j];
-            cv::rectangle(img_datas[i], cv::Rect(plate.x, plate.y, plate.width, plate.height), colors.at(plate.color));
-            cv::putText(img_datas[i], plate.text, cv::Point(plate.x, plate.y), 1, 0.8, colors.at(plate.color));
+            cv::rectangle(img_datas[i], cv::Rect(plate.x, plate.y, plate.width, plate.height), colors.at(plate.color), 2);
+            //cv::putText(img_datas[i], plate.text, cv::Point(plate.x, plate.y), 1, 0.8, colors.at(plate.color));
+            ft2->putText(img_datas[i], plate.text, cv::Point(plate.x, plate.y), 20, colors.at(plate.color), cv::FILLED, cv::LINE_AA, true);
         }
         
     }
     cv::imshow("image", image);
     cv::imshow("image2", image2);
+    cv::imshow("image3", image3);
     
     cv::waitKey(0);
     delete plateDetector;
--- third_party/trt_vehicle/models/vehicle_plate_detector.h
+++ third_party/trt_vehicle/models/vehicle_plate_detector.h
@@ -35,6 +35,6 @@ namespace trt_vehicle{
         std::vector<std::string> chars = {"0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F","G","H","WJ","J",\
             "K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","云","粤","鄂","豫","渝","新","湘","皖","苏","陕",\
             "琼","青","宁","闽","蒙","鲁","辽","京","晋","津","冀","吉","沪","黑","桂","贵","赣","甘","浙","川","藏","挂","学","警","防"};
-        std::vector<std::string> colors = {"blue", "yellow", "green", "white"};
+        std::vector<std::string> colors = {"blue", "green", "yellow", "white"};
     };
 }
