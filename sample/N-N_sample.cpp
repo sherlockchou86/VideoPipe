22ed76d41a1793c4ccbfec30ecbc4d16aa591d87
sherlockchou86 <zhouzhi9@foxmail.com>
2022-10-19 11:13:45 +0800

    add sample code and sample/readme(remove ./main)

---
+++ sample/N-N_sample.cpp
@@ -0,0 +1,59 @@
+
+#include "VP.h"
+
+#include "../nodes/vp_file_src_node.h"
+#include "../nodes/infers/vp_yunet_face_detector_node.h"
+#include "../nodes/infers/vp_sface_feature_encoder_node.h"
+#include "../nodes/osd/vp_face_osd_node_v2.h"
+#include "../nodes/vp_screen_des_node.h"
+#include "../nodes/vp_rtmp_des_node.h"
+
+#include "../utils/analysis_board/vp_analysis_board.h"
+
+/*
+* ## N-N sample ##
+* multi pipe exist separately and each pipe is 1-1-1 (can be any structure like 1-1-N, 1-N-N)
+*/
+
+#if _N_N_sample
+
+int main() {
+    VP_SET_LOG_INCLUDE_CODE_LOCATION(false);
+    VP_SET_LOG_INCLUDE_THREAD_ID(false);
+    VP_LOGGER_INIT();
+
+    // create nodes
+    auto file_src_0 = std::make_shared<vp_nodes::vp_file_src_node>("file_src_0", 0, "./test_video/10.mp4", 0.6);
+    auto yunet_face_detector_0 = std::make_shared<vp_nodes::vp_yunet_face_detector_node>("yunet_face_detector_0", "./models/face/face_detection_yunet_2022mar.onnx");
+    auto sface_face_encoder_0 = std::make_shared<vp_nodes::vp_sface_feature_encoder_node>("sface_face_encoder_0", "./models/face/face_recognition_sface_2021dec.onnx");
+    auto osd_0 = std::make_shared<vp_nodes::vp_face_osd_node_v2>("osd_0");
+    auto screen_des_0 = std::make_shared<vp_nodes::vp_screen_des_node>("screen_des_0", 0);
+
+    auto file_src_1 = std::make_shared<vp_nodes::vp_file_src_node>("file_src_1", 0, "./test_video/9.mp4", 0.6);
+    auto yunet_face_detector_1 = std::make_shared<vp_nodes::vp_yunet_face_detector_node>("yunet_face_detector_1", "./models/face/face_detection_yunet_2022mar.onnx");
+    auto sface_face_encoder_1 = std::make_shared<vp_nodes::vp_sface_feature_encoder_node>("sface_face_encoder_1", "./models/face/face_recognition_sface_2021dec.onnx");
+    auto osd_1 = std::make_shared<vp_nodes::vp_face_osd_node_v2>("osd_1");
+    auto screen_des_1 = std::make_shared<vp_nodes::vp_screen_des_node>("screen_des_1", 0);
+
+    // construct pipeline
+    // pipe 0
+    yunet_face_detector_0->attach_to({file_src_0});
+    sface_face_encoder_0->attach_to({yunet_face_detector_0});
+    osd_0->attach_to({sface_face_encoder_0});
+    screen_des_0->attach_to({osd_0});
+
+    // pipe 1
+    yunet_face_detector_1->attach_to({file_src_1});
+    sface_face_encoder_1->attach_to({yunet_face_detector_1});
+    osd_1->attach_to({sface_face_encoder_1});
+    screen_des_1->attach_to({osd_1});
+
+    file_src_0->start();
+    file_src_1->start();
+
+    // for debug purpose
+    vp_utils::vp_analysis_board board({file_src_0, file_src_1});
+    board.display();
+}
+
+#endif
---
+++ sample/readme.md
@@ -0,0 +1,43 @@
+
+## 1-1-1_sample ##
+1 video input, 1 infer task, and 1 output.
+![](../doc/p10.png)
+
+## 1-1-N_sample ##
+1 video input, 1 infer task, and 2 outputs.
+![](../doc/p11.png)
+
+
+## 1-N-N_sample ##
+1 video input and then split into 2 branches for different infer tasks, then 2 total outputs.
+![](../doc/p12.png)
+
+
+## N-1-N_sample ##
+2 video input and merge into 1 branch automatically for 1 infer task, then resume to 2 branches for outputs again.
+![](../doc/p13.png)
+
+
+## N-N_sample ##
+multi pipe exist separately and each pipe is 1-1-1 (can be any structure like 1-1-N, 1-N-N)
+![](../doc/p14.png)
+
+
+## paddle_infer_sample ##
+ocr based on paddle (install paddle_inference first!), 1 video input and 2 outputs (screen, rtmp)
+![](../doc/p15.png)
+
+
+## src_des_sample ##
+show how src nodes and des nodes work.
+3 (file, rtsp, udp) input and merge into 1 infer task, then resume to 3 branches for outputs (screen, rtmp, fake)
+![](../doc/p16.png)
+
+
+## trt_infer_sample ##
+vehicle and plate detector based on tensorrt (install tensorrt first!), 1 video input and 3 outputs (screen, file, rtmp)
+![](../doc/p17.png)
+
+
+## vp_logger_sample ##
+show how vp_logger works.
